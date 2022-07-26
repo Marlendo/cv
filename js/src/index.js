a95697f39dab6a8e72e0f90857b99e01ed602b7d
Marlendo <erikmarlendo@gmail.com>
2022-07-26 12:12:27 +0700

    initial commit

---
+++ js/src/index.js
@@ -0,0 +1,365 @@
+const constans = {
+    name: 'Erick Marlendo Novianto',
+    phone: '628999064808',
+    skill: [
+        {
+            title: 'HTML & CSS',
+            point: 100
+        },
+        {
+            title: 'Node JS',
+            point: 90
+        },
+        {
+            title: 'Angular',
+            point: 80
+        },
+        {
+            title: 'React Native',
+            point: 90
+        },
+        {
+            title: 'Flutter',
+            point: 80
+        },
+        {
+            title: 'PHP',
+            point: 80
+        },
+        {
+            title: 'Codeigniter',
+            point: 80
+        },
+        {
+            title: 'Electron JS',
+            point: 80
+        },
+        {
+            title: 'Corelldraw',
+            point: 100
+        },
+        {
+            title: 'Photoshop',
+            point: 70
+        },
+        {
+            title: 'IONIC',
+            point: 90
+        },
+        {
+            title: 'Linux Server',
+            point: 80
+        },
+        {
+            title: 'Teamwork',
+            point: 100
+        },
+        {
+            title: 'Individual Work',
+            point: 100
+        }
+    ],
+    services: [
+        {
+            title: 'Frontend developer',
+            desc: 'Gatsby js and react js or angular latest version or another frontend framework what you want',
+            icon: 'icon_tools'
+        },
+        {
+            title: 'Backend developer',
+            desc: 'Node js express with sequlize and all database whatever what you want',
+            icon: 'icon_cloud-download'
+        },
+        {
+            title: 'Fullstack Javascript Developer',
+            desc: 'all frontend and backend api service using javacript newest tectonogy',
+            icon: 'icon_globe'
+        },
+        {
+            title: 'Mobile Developer Android / IOS',
+            desc: 'Bot android or IOS using cross platform tectonogy using react native or flutter',
+            icon: 'icon_archive'
+        },
+    ],
+    portfolio: [
+        {
+            category: 'mobile',
+            img: 'images/pf_hs.png',
+            title: 'Home Services App',
+            desc: 'Cantik Homeservices Apps is online salon and massage services using react native'
+        },
+        {
+            category: 'mobile',
+            img: 'images/pf_untung.png',
+            title: 'Untung Apps',
+            desc: 'Untung apps is market place apps owned by cv untung sukses abadi build using ionic 3 framework'
+        },
+        {
+            category: 'mobile',
+            img: 'images/pf_mobo.png',
+            title: 'Saldo Mobo Apps',
+            desc: 'Indosat Saldo Mobo Apps is mobile apps from indosat for inject voucher and soon for help facilitate the work of partners outlet.'
+        },
+        {
+            category: 'mobile',
+            img: 'images/pf_weplay.png',
+            title: 'Weplay Apps',
+            desc: 'Weplay Apps is application helps make it easier for someone to book online sports fields.'
+        },
+        {
+            category: 'web',
+            img: 'images/pf_web_emoji.png',
+            title: 'Emoji Apps',
+            desc: 'Emoji Apps is web application OCR system for internal usage owned by bca bank'
+        },
+        {
+            category: 'web',
+            img: 'images/pf_web_ucc.png',
+            title: 'UCC Web Apps',
+            desc: 'UCC Web Apps is universal campus consortium system for unite all campus systems in the world'
+        },
+        {
+            category: 'web',
+            img: 'images/pf_web_hs.png',
+            title: 'Landing Page Cantik Homeservices',
+            desc: 'This website is for introduce cantik homeservices apps build using html css and js'
+        }
+    ],
+    resume: [
+        {
+            title: 'Fullstack developer',
+            subtitle: 'Startup weplay.id',
+            icon: 'icon_toolbox_alt',
+            time: 'January 2020 - Present (5 months)',
+            desc: 'android / ios development using React Native and backend using node js'
+        },
+        {
+            title: 'Frontend developer',
+            subtitle: 'PT Phincon ( phintraco consulting )',
+            icon: 'icon_toolbox_alt',
+            time: 'June 2019 - December 2019 (7 months)',
+            desc: 'development web apps bank bca OCR system at slipi, jakarta using react js'
+        },
+        {
+            title: 'Mobile apps developer ( android )',
+            subtitle: 'PT Phincon ( phintraco consulting )',
+            icon: 'icon_toolbox_alt',
+            time: 'July 2018 - July 2019 (a year)',
+            desc: 'Development mobile apps for indosat saldo mobo at central Jakarta, Indonesia using angular js and ionic'
+        },
+        {
+            title: 'Fullstack Mobile apps developer',
+            subtitle: 'PT Erporate Solusi Global ( Yogyakarta )',
+            icon: 'icon_toolbox_alt',
+            time: 'December 2017 - June 2018 (7 months)',
+            desc: 'Development Android apps and backend api for marketplace application android / ios development using ionic and backend using codeigniter'
+        },
+        {
+            title: 'Maccine technician ( cpsb css 3000 molding bootle maker )',
+            subtitle: 'PT Kiewell Plastick Packaging',
+            icon: 'icon_toolbox_alt',
+            time: 'November 2016 - December 2017 (1 year 1 month)',
+            desc: 'not programing but this job is fun ...'
+        },
+        {
+            title: 'Graphic designer 2D',
+            subtitle: 'PT Kiewell Plastick Packaging',
+            icon: 'icon_toolbox_alt',
+            time: 'May 2015 - September 2016 (1 year 8 month)',
+            desc: 'my first job is grapic designer 2d using correldraw and photoshop'
+        },
+        {
+            title: 'Bachelor Degree',
+            subtitle: 'Stmik Duta Bangsa Surakarta',
+            icon: 'icon_contacts_alt',
+            time: 'April 2013 - April 2017 (4 years)',
+            desc: '01 Bachelor Degree Informatics Engineering'
+        }
+    ]
+};
+
+
+function fillData() {
+    const eName = document.getElementsByClassName('dataName');
+    for (let row of eName) {
+        row.innerHTML = constans.name
+    }
+}
+
+function skillData() {
+    const element = document.getElementById('element-skill');
+    let skill = '';
+    for (let row of constans.skill) {
+        skill = skill + `
+        <div class="skill-outer">
+            <h4>${row.title}<span>${row.point}%</span></h4>
+            <div class="progress progress-striped active">
+                <div class="progress-bar" role="progressbar" aria-valuenow="${row.point}" aria-valuemin="0" aria-valuemax="100" style="width: ${row.point}%;"></div>
+            </div>
+        </div>     
+        `
+    }
+    element.innerHTML = skill
+}
+
+function servicesData() {
+    const element = document.getElementById('element-services')
+    let services = '';
+    for (let row of constans.services) {
+        services = services + `
+                        <div class="service-box">
+                            <div>
+                                <i class="icon ${row.icon}"></i>
+                            </div>
+                            <div>
+                                <h3>${row.title}</h3>
+                                <p>${row.desc}</p>
+                            </div>
+                        </div>      
+        `
+    }
+    element.innerHTML = services;
+}
+
+function portfolioData() {
+    const element = document.getElementById('element-portfolio')
+    let pf = ''
+    for (let i in constans.portfolio) {
+        let row = constans.portfolio[i];
+        pf = pf + `
+        <div class="col-xs-12 col-sm-4 mix ${row.category}">
+            <div class="imageWrapper">
+                <a class="lightbox" href="#popup-${i}">
+                    <img src="${row.img}" alt="${row.title}">
+                    <div class="imageHover">
+                        <div>
+                            <h3>${row.title}</h3>
+                            <span>${row.category + ' Apps'}</span>
+                        </div>
+                    </div>
+                </a>
+                <div id="popup-${i}" class="mfp-hide popup-box">
+                    <img src="${row.img}" alt="">
+                    <div>
+                        <h3>${row.title}</h3>
+                        <p>${row.desc}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        `;
+        element.innerHTML = pf;
+    }
+}
+
+function resumeData() {
+    const element = document.getElementById('element-resume');
+    let resume = ''
+    for (let row of constans.resume) {
+        resume = resume + `
+            <div class="resume-box">
+                <div>
+                    <i class="${row.icon}"></i>
+                </div>
+                <div>
+                    <h3>${row.title}</h3>
+                    <span>${row.time}</span>
+                    <p><b>${row.subtitle}</b></p>
+                    <p>${row.desc}</p>
+                </div>
+            </div>            
+        `
+    }
+    element.innerHTML = resume;
+}
+
+// whatsappme--dialog
+
+function checkMobile() {
+    if (window.navigator.userAgent.match(/Android/i)
+        || window.navigator.userAgent.match(/webOS/i)
+        || window.navigator.userAgent.match(/iPhone/i)
+        || window.navigator.userAgent.match(/iPad/i)
+        || window.navigator.userAgent.match(/iPod/i)
+        || window.navigator.userAgent.match(/BlackBerry/i)
+        || window.navigator.userAgent.match(/Windows Phone/i)
+    ) {
+        return true
+    } else {
+        return false
+    }
+
+}
+
+function whatsappOpen() {
+    const element = document.getElementById('whatsapp-dialog')
+    if (element.classList.contains('whatsappme--dialog')) {
+        let link = 'https://api.whatsapp.com/send?text=';
+        if(checkMobile()){
+            link = 'whatsapp://send?text=';
+        }
+        const message = 'Hi, Erick';    
+        const wa = link + message + '&phone=' + constans.phone;
+        window.open(wa);
+    } else {
+        element.classList.add('whatsappme--dialog')
+    }
+}
+
+function whatsappClose() {
+    const element = document.getElementById('whatsapp-dialog')
+    element.classList.remove('whatsappme--dialog')
+}
+
+function buildWhatsapp() {
+    const element = document.getElementById("element-whatsapp")
+    element.innerHTML = `
+    <div id="whatsapp-dialog" class="whatsappme whatsappme--left whatsappme--show">		
+        <div onclick="whatsappOpen()" class="whatsappme__button">
+            <svg class="whatsappme__button__open" viewBox="0 0 24 24">
+                <path fill="#fff"
+                    d="M3.516 3.516c4.686-4.686 12.284-4.686 16.97 0 4.686 4.686 4.686 12.283 0 16.97a12.004 12.004 0 0 1-13.754 2.299l-5.814.735a.392.392 0 0 1-.438-.44l.748-5.788A12.002 12.002 0 0 1 3.517 3.517zm3.61 17.043l.3.158a9.846 9.846 0 0 0 11.534-1.758c3.843-3.843 3.843-10.074 0-13.918-3.843-3.843-10.075-3.843-13.918 0a9.846 9.846 0 0 0-1.747 11.554l.16.303-.51 3.942a.196.196 0 0 0 .219.22l3.961-.501zm6.534-7.003l-.933 1.164a9.843 9.843 0 0 1-3.497-3.495l1.166-.933a.792.792 0 0 0 .23-.94L9.561 6.96a.793.793 0 0 0-.924-.445 1291.6 1291.6 0 0 0-2.023.524.797.797 0 0 0-.588.88 11.754 11.754 0 0 0 10.005 10.005.797.797 0 0 0 .88-.587l.525-2.023a.793.793 0 0 0-.445-.923L14.6 13.327a.792.792 0 0 0-.94.23z">
+                </path>
+            </svg>
+            <svg class="whatsappme__button__send" viewBox="0 0 400 400" fill="none" fill-rule="evenodd" stroke="#fff"
+                stroke-linecap="round" stroke-width="33">
+                <path class="wame_plain" stroke-dasharray="1096.67" stroke-dashoffset="1096.67"
+                    d="M168.83 200.504H79.218L33.04 44.284a1 1 0 0 1 1.386-1.188L365.083 199.04a1 1 0 0 1 .003 1.808L34.432 357.903a1 1 0 0 1-1.388-1.187l29.42-99.427">
+                </path>
+                <path class="wame_chat" stroke-dasharray="1019.22" stroke-dashoffset="1019.22"
+                    d="M318.087 318.087c-52.982 52.982-132.708 62.922-195.725 29.82l-80.449 10.18 10.358-80.112C18.956 214.905 28.836 134.99 81.913 81.913c65.218-65.217 170.956-65.217 236.174 0 42.661 42.661 57.416 102.661 44.265 157.316">
+                </path>
+            </svg>
+            <div class="whatsappme__badge">1</div>
+        </div>
+        <div class="whatsappme__box">
+            <div class="whatsappme__header">
+                <svg viewBox="0 0 120 28">
+                    <path fill="#fff" fill-rule="evenodd"
+                        d="M117.2 17c0 .4-.2.7-.4 1-.1.3-.4.5-.7.7l-1 .2c-.5 0-.9 0-1.2-.2l-.7-.7a3 3 0 0 1-.4-1 5.4 5.4 0 0 1 0-2.3c0-.4.2-.7.4-1l.7-.7a2 2 0 0 1 1.1-.3 2 2 0 0 1 1.8 1l.4 1a5.3 5.3 0 0 1 0 2.3zm2.5-3c-.1-.7-.4-1.3-.8-1.7a4 4 0 0 0-1.3-1.2c-.6-.3-1.3-.4-2-.4-.6 0-1.2.1-1.7.4a3 3 0 0 0-1.2 1.1V11H110v13h2.7v-4.5c.4.4.8.8 1.3 1 .5.3 1 .4 1.6.4a4 4 0 0 0 3.2-1.5c.4-.5.7-1 .8-1.6.2-.6.3-1.2.3-1.9s0-1.3-.3-2zm-13.1 3c0 .4-.2.7-.4 1l-.7.7-1.1.2c-.4 0-.8 0-1-.2-.4-.2-.6-.4-.8-.7a3 3 0 0 1-.4-1 5.4 5.4 0 0 1 0-2.3c0-.4.2-.7.4-1 .1-.3.4-.5.7-.7a2 2 0 0 1 1-.3 2 2 0 0 1 1.9 1l.4 1a5.4 5.4 0 0 1 0 2.3zm1.7-4.7a4 4 0 0 0-3.3-1.6c-.6 0-1.2.1-1.7.4a3 3 0 0 0-1.2 1.1V11h-2.6v13h2.7v-4.5c.3.4.7.8 1.2 1 .6.3 1.1.4 1.7.4a4 4 0 0 0 3.2-1.5c.4-.5.6-1 .8-1.6.2-.6.3-1.2.3-1.9s-.1-1.3-.3-2c-.2-.6-.4-1.2-.8-1.6zm-17.5 3.2l1.7-5 1.7 5h-3.4zm.2-8.2l-5 13.4h3l1-3h5l1 3h3L94 7.3h-3zm-5.3 9.1l-.6-.8-1-.5a11.6 11.6 0 0 0-2.3-.5l-1-.3a2 2 0 0 1-.6-.3.7.7 0 0 1-.3-.6c0-.2 0-.4.2-.5l.3-.3h.5l.5-.1c.5 0 .9 0 1.2.3.4.1.6.5.6 1h2.5c0-.6-.2-1.1-.4-1.5a3 3 0 0 0-1-1 4 4 0 0 0-1.3-.5 7.7 7.7 0 0 0-3 0c-.6.1-1 .3-1.4.5l-1 1a3 3 0 0 0-.4 1.5 2 2 0 0 0 1 1.8l1 .5 1.1.3 2.2.6c.6.2.8.5.8 1l-.1.5-.4.4a2 2 0 0 1-.6.2 2.8 2.8 0 0 1-1.4 0 2 2 0 0 1-.6-.3l-.5-.5-.2-.8H77c0 .7.2 1.2.5 1.6.2.5.6.8 1 1 .4.3.9.5 1.4.6a8 8 0 0 0 3.3 0c.5 0 1-.2 1.4-.5a3 3 0 0 0 1-1c.3-.5.4-1 .4-1.6 0-.5 0-.9-.3-1.2zM74.7 8h-2.6v3h-1.7v1.7h1.7v5.8c0 .5 0 .9.2 1.2l.7.7 1 .3a7.8 7.8 0 0 0 2 0h.7v-2.1a3.4 3.4 0 0 1-.8 0l-1-.1-.2-1v-4.8h2V11h-2V8zm-7.6 9v.5l-.3.8-.7.6c-.2.2-.7.2-1.2.2h-.6l-.5-.2a1 1 0 0 1-.4-.4l-.1-.6.1-.6.4-.4.5-.3a4.8 4.8 0 0 1 1.2-.2 8.3 8.3 0 0 0 1.2-.2l.4-.3v1zm2.6 1.5v-5c0-.6 0-1.1-.3-1.5l-1-.8-1.4-.4a10.9 10.9 0 0 0-3.1 0l-1.5.6c-.4.2-.7.6-1 1a3 3 0 0 0-.5 1.5h2.7c0-.5.2-.9.5-1a2 2 0 0 1 1.3-.4h.6l.6.2.3.4.2.7c0 .3 0 .5-.3.6-.1.2-.4.3-.7.4l-1 .1a21.9 21.9 0 0 0-2.4.4l-1 .5c-.3.2-.6.5-.8.9-.2.3-.3.8-.3 1.3s.1 1 .3 1.3c.1.4.4.7.7 1l1 .4c.4.2.9.2 1.3.2a6 6 0 0 0 1.8-.2c.6-.2 1-.5 1.5-1a4 4 0 0 0 .2 1H70l-.3-1v-1.2zm-11-6.7c-.2-.4-.6-.6-1-.8-.5-.2-1-.3-1.8-.3-.5 0-1 .1-1.5.4a3 3 0 0 0-1.3 1.2v-5h-2.7v13.4H53v-5.1c0-1 .2-1.7.5-2.2.3-.4.9-.6 1.6-.6.6 0 1 .2 1.3.6.3.4.4 1 .4 1.8v5.5h2.7v-6c0-.6 0-1.2-.2-1.6 0-.5-.3-1-.5-1.3zm-14 4.7l-2.3-9.2h-2.8l-2.3 9-2.2-9h-3l3.6 13.4h3l2.2-9.2 2.3 9.2h3l3.6-13.4h-3l-2.1 9.2zm-24.5.2L18 15.6c-.3-.1-.6-.2-.8.2A20 20 0 0 1 16 17c-.2.2-.4.3-.7.1-.4-.2-1.5-.5-2.8-1.7-1-1-1.7-2-2-2.4-.1-.4 0-.5.2-.7l.5-.6.4-.6v-.6L10.4 8c-.3-.6-.6-.5-.8-.6H9c-.2 0-.6.1-.9.5C7.8 8.2 7 9 7 10.7c0 1.7 1.3 3.4 1.4 3.6.2.3 2.5 3.7 6 5.2l1.9.8c.8.2 1.6.2 2.2.1.6-.1 2-.8 2.3-1.6.3-.9.3-1.5.2-1.7l-.7-.4zM14 25.3c-2 0-4-.5-5.8-1.6l-.4-.2-4.4 1.1 1.2-4.2-.3-.5A11.5 11.5 0 0 1 22.1 5.7 11.5 11.5 0 0 1 14 25.3zM14 0A13.8 13.8 0 0 0 2 20.7L0 28l7.3-2A13.8 13.8 0 1 0 14 0z">
+                    </path>
+                </svg>
+                <div onclick="whatsappClose()" class="whatsappme__close"><svg viewBox="0 0 24 24">
+                    <path fill="#fff"
+                        d="M24 2.4L21.6 0 12 9.6 2.4 0 0 2.4 9.6 12 0 21.6 2.4 24l9.6-9.6 9.6 9.6 2.4-2.4-9.6-9.6L24 2.4z">
+                    </path>
+                </svg></div>
+            </div>
+            <div class="whatsappme__message">Hi, My Name is <b>Erick Marlendo Noviyanto</b> iam fullstack javascript specialis, do you want to hire me ?
+            </div>            
+        </div>
+    </div>
+    `
+}
+
+
+function main() {
+    fillData()
+    skillData()
+    servicesData()
+    portfolioData()
+    resumeData()
+    buildWhatsapp()
+}
+
+main()
